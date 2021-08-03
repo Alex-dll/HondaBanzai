900c2ed34f84bc286afab9c644808e21a0a2d879
Alex <alexsandrojr.303@gmail.com>
2021-08-03 18:55:13 -0300

    add Responsivit to FirstSight

--- src/components/Header/FirstSight/index.tsx
+++ src/components/Header/FirstSight/index.tsx
@@ -1,12 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
+import styles from './styles.module.scss'
 
 export function FirstSight() {
   return (
-    <section>
-      <img src="/images/logo.png" alt="Honda Banzai" />
-      <div>
-        <p>CENTRAL DE ATENDIMENTO</p>
-        <strong>(31) 3123-1234</strong>
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <img src="/images/logo.png" alt="Honda Banzai" />
+        <div>
+          <p>CENTRAL DE ATENDIMENTO</p>
+          <strong>(31) 3123-1234</strong>
+        </div>
       </div>
     </section>
   )
