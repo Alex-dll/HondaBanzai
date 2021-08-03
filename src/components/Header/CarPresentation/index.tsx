b0dde326bfc410dbe8218ee63d27f0512f0a65ea
Alex <alexsandrojr.303@gmail.com>
2021-08-03 19:22:51 -0300

    add responsivity to CarPresentation

--- src/components/Header/CarPresentation/index.tsx
+++ src/components/Header/CarPresentation/index.tsx
@@ -4,6 +4,7 @@ import styles from './styles.module.scss'
 export function CarPresentation() {
   return (
     <section className={styles.container}>
+        <div></div>
         <img 
           src="/images/mobile/Civic_EXL_2017_3_4_Lado_A_Preto_72dpi@2x.png" 
           alt="Carro Civic" 
