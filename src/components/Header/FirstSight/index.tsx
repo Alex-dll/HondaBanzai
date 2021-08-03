33bb00dd22c37e4804ada96816bab62f215d88d8
Alex <alexsandrojr.303@gmail.com>
2021-08-03 18:28:50 -0300

    add FirstSight component

---
+++ src/components/Header/FirstSight/index.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
+
+export function FirstSight() {
+  return (
+    <section>
+      <img src="/images/logo.png" alt="Honda Banzai" />
+      <div>
+        <p>CENTRAL DE ATENDIMENTO</p>
+        <strong>(31) 3123-1234</strong>
+      </div>
+    </section>
+  )
+} 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,11 @@
+import { FirstSight } from "./FirstSight";
 import { Navigator } from "./Navigator";
 
 export function Header() {
   return (
     <header>
       <Navigator />
+      <FirstSight />
     </header>
   )
 }
