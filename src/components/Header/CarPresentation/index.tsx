77d17381e4636b69b2b3a95bb7aadef153460433
Alex <alexsandrojr.303@gmail.com>
2021-08-03 19:02:56 -0300

    add carPresentation

---
+++ src/components/Header/CarPresentation/index.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from './styles.module.scss'
+
+export function CarPresentation() {
+  return (
+    <section className={styles.container}>
+        <img 
+          src="/images/mobile/Civic_EXL_2017_3_4_Lado_A_Preto_72dpi@2x.png" 
+          alt="Carro Civic" 
+          className={styles.car}
+        />
+        <img 
+          src="/images/mobile/Grupo 98.svg"
+          alt="Carro Civic" 
+          className={styles.nameCar}
+        />
+    </section>
+  )
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { CarPresentation } from "./CarPresentation";
 import { FirstSight } from "./FirstSight";
 import { Navigator } from "./Navigator";
 
@@ -6,6 +7,7 @@ export function Header() {
     <header>
       <Navigator />
       <FirstSight />
+      <CarPresentation />
     </header>
   )
 }
