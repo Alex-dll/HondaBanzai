1ae3481ba48a3e972f672a2fdd709b9bda24d8fa
Alex <alexsandrojr.303@gmail.com>
2021-08-04 02:59:52 -0300

    add Componente and responsive GetHonda

---
+++ src/components/Civic/GetHonda/index.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from './styles.module.scss'
+
+export function GetHonda() {
+
+  return (
+    <section className={styles.container}>
+      <h1>PRONTO PARA TER UM HONDA?</h1>
+      <p>Conheça os últimos grandes lançamentos da honda.</p>
+      <div className={styles.cards}>
+      <div className={styles.card}>
+        <div className={styles.cardCar}>
+          <div className={styles.cardCarMobile}>
+            <img src="/images/accord@2x.png" alt="Honda Accord" />
+            <p>Novo Honda Accord: a combinação perfeita de três atributos exigidos pelo consumidor: elegância, luxo e alto desempenho.</p>
+            <button><img src="/images/mobile/chevron-down.svg" alt="Seta pra Baixo" /></button>
+            <button className={styles.buttonDesktop}>CONHEÇA MAIS</button>
+          </div>
+        </div>
+            <button className={styles.buttonMobile}>CONHEÇA MAIS</button>
+      </div>
+      <div className={`${styles.card} ${styles.cardsDesktop}`}>
+        <div className={styles.cardCar}>
+          <div className={styles.cardCarMobile}>
+            <img src="/images/accord@2x.png" alt="Honda Accord" />
+            <p>Novo Honda Accord: a combinação perfeita de três atributos exigidos pelo consumidor: elegância, luxo e alto desempenho.</p>
+            <button><img src="/images/mobile/chevron-down.svg" alt="Seta pra Baixo" /></button>
+            <button className={styles.buttonDesktop}>CONHEÇA MAIS</button>
+          </div>
+        </div>
+            <button className={styles.buttonMobile}>CONHEÇA MAIS</button>
+      </div>
+      <div className={`${styles.card} ${styles.cardsDesktop}`}>
+        <div className={styles.cardCar}>
+          <div className={styles.cardCarMobile}>
+            <img src="/images/accord@2x.png" alt="Honda Accord" />
+            <p>Novo Honda Accord: a combinação perfeita de três atributos exigidos pelo consumidor: elegância, luxo e alto desempenho.</p>
+            <button><img src="/images/mobile/chevron-down.svg" alt="Seta pra Baixo" /></button>
+            <button className={styles.buttonDesktop}>CONHEÇA MAIS</button>
+          </div>
+        </div>
+            <button className={styles.buttonMobile}>CONHEÇA MAIS</button>
+      </div>
+      </div>
+    </section>
+  )
+}
--- src/components/Civic/index.tsx
+++ src/components/Civic/index.tsx
@@ -1,11 +1,14 @@
 import { Cars } from "./Cars";
 import { FrontPanel } from "./FrontPanel";
+import { GetHonda } from "./GetHonda";
 
 export function Civic() {
   return (
     <>
       <Cars />
       <FrontPanel />
+
+      <GetHonda />
     </>
   )
 }
