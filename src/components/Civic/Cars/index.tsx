b1afa0a3785d320046eb99d764f3bd272687e5d1
Alex <alexsandrojr.303@gmail.com>
2021-08-03 21:28:13 -0300

    add responsivity to Civic

--- src/components/Civic/Cars/index.tsx
+++ src/components/Civic/Cars/index.tsx
@@ -1,16 +1,17 @@
+/* eslint-disable @next/next/no-img-element */
 import styles from './styles.module.scss'
 
 export function Cars() {
   return (
-    <section>
+    <section className={styles.container}>
       <h1>A EMOÇÃO DE DIRIGIR LEVA VOCÊ MAIS LONGE</h1>
       <div>
         <aside>
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
+          <img src="/images/desktop/Retângulo 16@2x.png" alt="Lateral do Carro" />
+          <img src="/images/desktop/Retângulo 19@2x.png" alt="Trazeira do Carro" />
+          <img src="/images/desktop/Retângulo 21@2x.png" alt="Frente do Carro" />
         </aside>
-          <img src="" alt="" />
+          <img src="/images/juntosi@2x.png" alt="2 Hondas" />
       </div>
       <p>
         O novo Civic Geração 10 traz um design único mudando a categoria de sedãs para sempre.
